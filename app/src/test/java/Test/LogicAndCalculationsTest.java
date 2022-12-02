72baaca65464f96660aac2a8f50af297d38297ec
colerusson <colerusson2@gmail.com>
2022-12-01 20:50:17 -0700

    Set up classes to write junit tests in

---
+++ app/src/test/java/Test/LogicAndCalculationsTest.java
@@ -0,0 +1,59 @@
+package Test;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class LogicAndCalculationsTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void takeDown() {
+
+    }
+
+    @Test
+    public void familyRelationshipTestPass() {
+
+    }
+
+    @Test
+    public void familyRelationshipTestFail() {
+
+    }
+
+    @Test
+    public void filterEventsTestPass() {
+
+    }
+
+    @Test
+    public void filterEventsTestFail() {
+
+    }
+
+    @Test
+    public void sortEventsTestPass() {
+
+    }
+
+    @Test
+    public void sortEventsTestFail() {
+
+    }
+
+    @Test
+    public void searchTestPass() {
+
+    }
+
+    @Test
+    public void searchTestFail() {
+
+    }
+
+}
---
+++ app/src/test/java/Test/ServerProxyTest.java
@@ -0,0 +1,59 @@
+package Test;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+public class ServerProxyTest {
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void takeDown() {
+
+    }
+
+    @Test
+    public void loginTestPass() {
+
+    }
+
+    @Test
+    public void loginTestFail() {
+
+    }
+
+    @Test
+    public void registerTestPass() {
+
+    }
+
+    @Test
+    public void registerTestFail() {
+
+    }
+
+    @Test
+    public void getPeopleTestPass() {
+
+    }
+
+    @Test
+    public void getPeopleTestFail() {
+
+    }
+
+    @Test
+    public void getEventsTestPass() {
+
+    }
+
+    @Test
+    public void getEventsTestFail() {
+
+    }
+
+}
