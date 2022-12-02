4113a1fa5e7b8425a42210bb5bd91d800dbc6ffa
colerusson <colerusson2@gmail.com>
2022-12-02 12:26:24 -0700

    Set up test class for calculations and logic testing

---
+++ app/src/test/java/Test/CalculationsTest.java
@@ -0,0 +1,61 @@
+package Test;
+
+import org.junit.*;
+
+import Data.DataCache;
+
+public class CalculationsTest {
+
+    static private final DataCache cache = DataCache.getInstance();
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @After
+    public void takeDown() {
+
+    }
+
+    @Test
+    public void relationshipTestPass() {
+
+    }
+
+    @Test
+    public void relationshipTestFail() {
+
+    }
+
+    @Test
+    public void settingsTestPass() {
+
+    }
+
+    @Test
+    public void settingsTestFail() {
+
+    }
+
+    @Test
+    public void sortEventsTestPass() {
+
+    }
+
+    @Test
+    public void sortEventsTestFail() {
+
+    }
+
+    @Test
+    public void searchTestPass() {
+
+    }
+
+    @Test
+    public void searchTestFail() {
+
+    }
+
+}
